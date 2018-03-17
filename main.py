08fa975bc290fa2f3e6c0561995e73b0a116b535
dongmin <4artit@gmail.com>
2018-03-18 00:36:33 +0900

    fixed some error

--- main.py
+++ main.py
@@ -79,9 +79,9 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     """
     # TODO: Implement function
     logits = tf.reshape(nn_last_layer, (-1, num_classes))
-    labels = tf.reshape(correct_label, (-1, num_classes))
+    correct_label = tf.reshape(correct_label, (-1, num_classes))
 
-    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, labels))
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits= logits, labels= correct_label))
     optimizer = tf.train.AdamOptimizer(learning_rate= learning_rate)
     train_op = optimizer.minimize(cross_entropy_loss)
     return logits, train_op, cross_entropy_loss
@@ -104,12 +104,12 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param learning_rate: TF Placeholder for learning rate
     """
     # TODO: Implement function
-    for epoch in epochs:
+    for epoch in range(epochs):
         print("EPOCH {}".format(epoch + 1))
         for image, label in get_batches_fn(batch_size):
             _, loss = sess.run([train_op, cross_entropy_loss],
             feed_dict = {input_image: image, correct_label: label, keep_prob: 0.5, learning_rate: 0.0001})
-            print("LOSS : {:.f}".format(loss))
+            print("LOSS : {:.3f}".format(loss))
 tests.test_train_nn(train_nn)
 
 
